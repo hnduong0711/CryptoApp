b29a7937af00ed772c238c7111157fdcddc84da6
DuongTienSinh <huynhnhutduong7112003@gmail.com>
2024-07-03 14:27:10 +0700

    feat: UI progress...

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <div class="hero">
+        <h1>Largest <br /> Crypto Marketplace </h1>
+        <p></p>
+      </div>
+    </div>
+  )
+}
+
+export default Home
